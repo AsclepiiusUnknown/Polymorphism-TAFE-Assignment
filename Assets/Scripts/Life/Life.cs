56d4303707c7478bcd8a06b2530d2295b8ef4670
AsclepiiusUnknown <55474351+AsclepiiusUnknown@users.noreply.github.com>
2020-06-06 11:28:40 +1000

    Yes

---
+++ Assets/Scripts/Life/Life.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    #region Variables
+    public Flock flock;
+
+    public float maxHealth = 100;
+    [HideInInspector]
+    public float health;
+    #endregion
+
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Life/Prey.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prey : Life
+{
+    public bool useHurtColor = true;
+    public Color hurtColor = Color.red;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        /*if (useHurtColor)
+        {
+            flock.agent.GetComponent<SpriteRenderer>().color = 
+        }*/
+    }
+
+    public void PlayHurtColorEffectVoid(SpriteRenderer renderer, int repeatAmount, float waitTime)
+    {
+        PlayHurtColorEffect(renderer, repeatAmount, waitTime);
+    }
+
+    public IEnumerator PlayHurtColorEffect(SpriteRenderer renderer, int repeatAmount, float waitTime)
+    {
+        Color tempStartColor = renderer.color;
+
+        for (int i = 0; i < repeatAmount; i++)
+        {
+            renderer.color = hurtColor;
+            yield return new WaitForSeconds(waitTime);
+            renderer.color = tempStartColor;
+        }
+    }
+}
